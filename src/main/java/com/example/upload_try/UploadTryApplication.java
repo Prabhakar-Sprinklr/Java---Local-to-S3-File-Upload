cb0ca0491da8e90bfaf474605020002b383afcdc
Prabhakar Kumar <prabhakar.kumar@sprinklr.com>
2022-01-16 12:49:28 +0530

    First Commit working with MAVEN

---
+++ src/main/java/com/example/upload_try/UploadTryApplication.java
@@ -0,0 +1,13 @@
+package com.example.upload_try;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UploadTryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UploadTryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/upload_try/UploadTryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.upload_try;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UploadTryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
